43855ba702b4c09b1d5d04896bad294fd69f771d
二师弟 <32268834@qq.com>
2015-01-29 19:33:37 +0800

    Update monitor.py

--- monitor.py
+++ monitor.py
@@ -12,11 +12,12 @@ m = Monitor()
 r = redis_count()
 c = mysql_select()
 queue = Queue()
+a = c.url_select()
 
 class ProducerThread(Thread):
         def run(self):
                 global queue
-                for x in c.url_select():
+                for x in a:
                         l = []
                         l.append(x)
                         l.append(m.check_url(x[2]))
@@ -25,17 +26,16 @@ class ProducerThread(Thread):
 
 class ConsumerThread(Thread):
         def run(self):
-                global queue
-                for x in c.url_select():
+                for x in a:
                         result = queue.get()
+                        print result
                         if result[1] == 0:
                                 r.redis_modify(result[0][0])
                                 continue
                         else:
-                                if r.redis_select(result[0][0]) < 3:
+                                if r.redis_select('%s' %result[0][0]) < 3:
                                         r.redis_insert(result[0][0])
                                 else:
-                                        print r.redis_select(result[0][0])
                                         for user in c.user_select(result[0][1]):
                                                 content = ''' 接口出问题了。。。。
 Group:  %s
@@ -43,10 +43,10 @@ Group:  %s
 检测失败超过3次,尽快检查以免影响服务。。  
 ''' %(result[0][1],result[0][2])
                                                 m.send_mail('URL检测失败',user[0],content)
-                                                r.redis_insert(result[0][0])
-                queue.task_done()
+                                        r.redis_insert(result[0][0])
+#                       queue.task_done()
 
 
-#由于url数量较多，这里用到了多线程
 ProducerThread().start()
+#time.sleep(1)
 ConsumerThread().start()
