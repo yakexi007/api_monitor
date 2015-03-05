453b7cc74adc67d9ea6b16d636a7a1a43d8eda19
二师弟 <32268834@qq.com>
2015-03-05 10:46:06 +0800

    更新url检测方式，逻辑更清晰，代码更简洁

--- check_moudel.py
+++ check_moudel.py
@@ -3,72 +3,53 @@
 
 import time
 import pycurl
+import urllib2
+import sys
 import StringIO
 import smtplib  
 import socket
 import json
+import hashlib
 from email.mime.text import MIMEText 
 #reload(sys)
 #sys.setdefaultencoding('utf-8')
 
 class Monitor:
-        def check_url(self,url):
-                c = pycurl.Curl()
-                b = StringIO.StringIO()
-                c.setopt(pycurl.WRITEFUNCTION, b.write)
-                c.setopt(c.URL, url)
-                c.setopt(pycurl.CONNECTTIMEOUT, 20) 
-                c.setopt(pycurl.TIMEOUT, 20) 
-                #c.setopt(pycurl.NOPROGRESS, 1) 
-                #c.setopt(pycurl.FORBID_REUSE, 1)
-                #c.setopt(pycurl.MAXREDIRS, 1)
-                c.setopt(pycurl.DNS_CACHE_TIMEOUT, 30)
-
+        def check_url(self,url):  #传过来的url是一个列表
+                if url[6] != '':    #判断是否添加header  有些url需要通过header传递参数
+                        send_headers = eval("{%s}" %url[6].encode('utf8'))  
+                        #print send_headers
+                else:
+                        send_headers = {}
+                request = urllib2.Request(url[3],headers=send_headers)
                 try:
-                        c.perform()
-                        http_code = c.getinfo(pycurl.HTTP_CODE)
-                        html=json.loads(b.getvalue())
-                        if isinstance(html,list):
-                                if http_code == 200:
-                                        return 0
-                                else:
-                                        return 1
-                        elif isinstance(html,dict):
-                                if http_code == 200 and "status" in html.keys() and html["status"] == 200: #api mvms组监控条件
-                                        return 0
-                                elif http_code == 200 and "response" in html.keys() and html["response"]["numFound"] > 0: #search组
-                                        print "search"
-                                        return 0
-                                elif http_code == 200: #不根据返回值字段的监控
+                        response = urllib2.urlopen(request,timeout=5)
+                        return_data = response.read()
+                        http_code = response.getcode()
+                        if url[7] != '':       #检测是否需要通过关键字判断url状态，如果关键字不为空，判断url返回值中是否包含关键字
+                                if return_data.find(url[7]) > 0:  #如果包含关键字  则url状态正常
                                         return 0
                                 else:
                                         return 1
                         else:
-                                return 1
-                        c.close()
-                except Exception, e:
-                        http_code = c.getinfo(pycurl.HTTP_CODE)
-                        if http_code == 200:  #返回值不是json串的监控
-                                return 0
-                        elif http_code != 200:
-                                return 1
-                        else:
-                                print "connection error:" + str(e)
-                        c.close()
+                                if http_code == 200:   #有些url只需通过httpcod判断状态
+                                        return 0
+                                else:
+                                        return 1
+          
+                except urllib2.URLError,e:  #url参数不正确会报错  在这抓取 防止程序运行报错
+                        return 1
 
-        def check_port(self,ip,port):
+        def check_port(self,x):  #端口检测
                 try:
                         sc=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
                         #设置超时时间（0.0）
                         sc.settimeout(2)
-                        sc.connect((ip,port))
+                        sc.connect((x[0],x[1]))
                         sc.close()
                         return 0
                 except:
-                        timenow=time.localtime()
-                        datenow = time.strftime('%Y-%m-%d %H:%M:%S', timenow)
-                        logstr="%s:%s 端口连接失败->%s \n" %(ip,port,datenow)
-                        return logstr
+                        return 1
 
         def send_mail(self,sub,user,content):
                 msg = MIMEText(content,_subtype = 'plain',_charset = 'gb2312')  
@@ -84,18 +65,20 @@ class Monitor:
                     return False
 
         def send_sms(self,phone,content):
-                time = time.strftime("%Y%m%d%H%M%S", time.localtime())
+                times = time.strftime("%Y%m%d%H%M%S", time.localtime())
+                with open('sms.txt','a+') as f:
+                        f.write(times + '\n')
+                        f.write(phone + '\n')
+                        f.write(content + '\n' + '\n')
                 appid = "100190"
                 key = "20140701104400402SMSPLATACCESS00" #验证key
                 priority = 3
                 m = hashlib.md5()
-                m.update(appid + phone + content + time + key)  #enc验证 需要appid 手机
+                m.update(appid + str(phone) + content + times + key)  #enc验证 需要appid 手机
                 md5 = m.hexdigest().encode('utf-8')           #将enc验证编码改为utf-8
-                content1 = urllib.quote(sys.argv[3].encode('utf-8','replace'))  #将中文内容改为url编码
+                content1 = urllib.quote(content.encode('utf-8','replace'))  #将中文内容改为url编码
 
-                URL = "http://demo.test.com/WLS/smsaccess/mt?appid=100190&destnumber=%s&content=%s&enc=%s&timestamp=%s&linkid=0&priority=3&tailsp=" %(phone,c
-ontent1,md5,time)
-
+                URL = "http://i.sms.sohu.com/WLS/smsaccess/mt?appid=100190&destnumber=%s&content=%s&enc=%s&timestamp=%s&linkid=0&priority=3&tailsp=" %(str(phone),content1,md5,times)
                 c = pycurl.Curl()
                 b = StringIO.StringIO()
                 c.setopt(pycurl.WRITEFUNCTION, b.write)
@@ -110,6 +93,6 @@ ontent1,md5,time)
                         html=b.getvalue()
                 except Exception,e:
                         c.close()
-
 if __name__ == "__main__":
+        m = Monitor()
         print "This is a moudel..."
