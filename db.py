8e0cc30ba0657fef3162309a00dd31b24555c06e
二师弟 <32268834@qq.com>
2015-03-05 13:56:59 +0800

    Update db.py

--- db.py
+++ db.py
@@ -3,24 +3,25 @@
 
 import redis
 import MySQLdb
+import time
 
-r = redis.Redis(host='127.0.0.1',port=6379)
-conn = MySQLdb.connect(host='127.0.0.1',user='root',passwd='123',db='monitor_url',port=3306)
+r = redis.Redis(host='10.10.10.10',port=6379)
+conn = MySQLdb.connect(host='127.0.0.1',user='xxx',passwd='xxx',db='webserver',port=3306,charset="utf8")
 cur = conn.cursor()
 
 class redis_count:
 
         def redis_select(self,id):
                 result = r[id]
-                return result
+                return int(result)
         def redis_insert(self,id):
                 r[id] = int(r[id]) + 1
         def redis_modify(self,id):
-                r[id] = 0
+                r[id] =  0
 
 class mysql_select:
         def url_select(self):
-                sql = "select * from url_info;"
+                sql = "select * from monitor_urlmodel;"
                 cur.execute(sql)
                 result = []
                 for x in cur.fetchall():
@@ -35,13 +36,22 @@ class mysql_select:
                         result.append(x)
                 return result
 
-        def user_select(self,gp):
-                sql = "select email,phone from user_info where gp = '%s';" %gp
-                cur.execute(sql)
+        def user_select(self,user):
                 result = []
-                for x in cur.fetchall():
-                        result.append(x)
+                for x in user.split(','):
+                        sql = "select email,phone from url_userinfo where xm = '%s';" %x
+                        cur.execute(sql)
+                        info = cur.fetchall()
+                        result.append(info[0])
                 return result
 
+        def update_time(self,id):
+                t = time.strftime('%Y-%m-%d %H:%M:%S',time.localtime(time.time()))
+                sql = "update monitor_urlmodel set lasttime = '%s' where id = '%s';" %(t,id)
+                cur.execute(sql)
+                conn.commit()
+
 if __name__ == '__main__':
+        m = mysql_select()
+        print m.user_select('api')
         print 'This is redis moudle...'
