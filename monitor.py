796453288ecccdbf560adb58444e0fe518a5b1ae
二师弟 <32268834@qq.com>
2015-03-27 15:35:32 +0800

    根据不同的返回值  发送不通的内容 ，新增恢复信息

--- monitor.py
+++ monitor.py
@@ -17,47 +17,92 @@ queue = Queue()
 a = c.url_select()
 
 class ProducerThread(Thread):
-        def run(self):
-                global queue
-                for x in a:
-                        l = []
-                        l.append(x)
-                        l.append(m.check_url(x))
-                        queue.put(l)
+    def run(self):
+        global queue
+        for x in a:
+            l = []
+            l.append(x)
+            l.append(m.check_url(x))
+            queue.put(l)
 
 
 class ConsumerThread(Thread):
-        def run(self):
-                global queue
-                for x in a:
-                        result = queue.get()
-                        print result
-                        if result[1] == 0:
-                                r.redis_modify(result[0][0])
-                                continue
-                        else:
-                                if r.redis_select(result[0][0]) < 3:
-                                        r.redis_insert(result[0][0])
-                                else:
-                                        if result[0][4] == 'on':
-                                                for user in c.user_select(result[0][8]):
-                                                        content = '''Group:  %s   URL :  %s  检测失败''' %(result[0][2],result[0][3])
-                                                        m.send_mail('URL检测失败',user[0],content)
-                                                        m.send_sms(user[1],content)
-                                                        #m.send_sms('18610941029',content)
-                                                times = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-                                                with open('sms.txt','a+') as f:  #记录日志
-                                                        f.write(times + '\n')
-                                                        f.write(content + '\n' + '\n')
-                                                c.update_time(result[0][0])
-                                                r.redis_insert(result[0][0])
-                                        else:
-                                                r.redis_insert(result[0][0])
-                                                continue
-#                       queue.task_done()
+    def run(self):
+        global queue
+        for x in a:
+            result = queue.get()
+            print result
+            if result[1] == 0:
+                if r.redis_select(result[0][0]) >3:
+                    for user in c.user_select(result[0][8]):
+                        content = '''Group:  %s   URL :  %s  恢复正常''' %(result[0][2],result[0][3])
+                        #m.send_mail('URL恢复正常',user[0],content)
+                        #m.send_sms(user[1],content)
+                    #m.send_sms('18610941029',content)
+                    #m.send_sms('13488852824',content)
+                    r.redis_modify(result[0][0])
+                else:
+                    #r.redis_modify(result[0][0])
+                    continue
+            elif result[1] == 1:
+                if r.redis_select(result[0][0]) < 3:
+                    r.redis_insert(result[0][0])
+                else:
+                    if result[0][4] == 'on':
+                        for user in c.user_select(result[0][8]):
+                            content = '''Group:  %s   URL :  %s  关键字检测失败''' %(result[0][2],result[0][3])
+                           # m.send_mail('URL检测失败',user[0],content)
+                           # m.send_sms(user[1],content)
+                        #m.send_sms('18610941029',content)
+                        #m.send_sms('13488852824',content)
+                        times = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                        with open('sms.txt','a+') as f:
+                            f.write(times + '\n')
+                            f.write(content + '\n' + '\n')
+                        c.update_time(result[0][0])
+                        r.redis_insert(result[0][0])
+                    else:
+                        r.redis_insert(result[0][0])
+            elif result[1] == 2:
+                if r.redis_select(result[0][0]) < 3:
+                    r.redis_insert(result[0][0])
+                else:
+                    if result[0][4] == 'on':
+                        for user in c.user_select(result[0][8]):
+                            content = '''Group:  %s   URL :  %s  返回状态码错误''' %(result[0][2],result[0][3])
+                           # m.send_mail('URL检测失败',user[0],content)
+                           # m.send_sms(user[1],content)
+                        #m.send_sms('18610941029',content)
+                        #m.send_sms('13488852824',content)
+                        times = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                        with open('sms.txt','a+') as f:
+                            f.write(times + '\n')
+                            f.write(content + '\n' + '\n')
+                        c.update_time(result[0][0])
+                        r.redis_insert(result[0][0])
+                    else:
+                        r.redis_insert(result[0][0])
+            else:
+                if r.redis_select(result[0][0]) < 3:
+                    r.redis_insert(result[0][0])
+                else:
+                    if result[0][4] == 'on':
+                        for user in c.user_select(result[0][8]):
+                            content = '''Group:  %s   URL :  %s  返回状态码错误''' %(result[0][2],result[0][3])
+                           # m.send_mail('URL检测失败',user[0],content)
+                           # m.send_sms(user[1],content)
+                        #m.send_sms('18610941029',content)
+                        #m.send_sms('13488852824',content)
+                        times = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+                        with open('sms.txt','a+') as f:
+                            f.write(times + '\n')
+                            f.write(content + '\n' + '\n')
+                        c.update_time(result[0][0])
+                        r.redis_insert(result[0][0])
+                    else:
+                        r.redis_insert(result[0][0])
 
-
+#queue.task_done()
 ProducerThread().start()
 #time.sleep(1)
 ConsumerThread().start()
-You have mail in /var/spool/mail/root
