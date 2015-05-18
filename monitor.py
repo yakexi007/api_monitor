110ed5bf73af85ad63e758dffd5eef6b13cf822b
二师弟 <32268834@qq.com>
2015-05-18 14:19:31 +0800

    比上个版本速度提高2s

--- monitor.py
+++ monitor.py
@@ -31,75 +31,48 @@ class ConsumerThread(Thread):
         global queue
         for x in a:
             result = queue.get()
-            print result
+            if result[0][4] != 'Yes':continue
             if result[1] == 0:
-                if r.redis_select(result[0][0]) >3:
+                if r.redis_select(result[0][0]) < 3:
+                    r.redis_modify(result[0][0])
+                else:
                     for user in c.user_select(result[0][8]):
                         content = '''Group:  %s   URL :  %s  恢复正常''' %(result[0][2],result[0][3])
                         m.send_mail('URL恢复正常',user[0],content)
                         m.send_sms(user[1],content)
-                    m.send_sms('186xxxxxxxx',content)
-                    m.send_sms('134xxxxxxxx',content)
+                    m.send_sms('18610941029',content)
+                    m.send_sms('13488852824',content)
                     r.redis_modify(result[0][0])
-                else:
-                    #r.redis_modify(result[0][0])
-                    continue
-            elif result[1] == 1:
+            elif result[1] != 0:
                 if r.redis_select(result[0][0]) < 3:
                     r.redis_insert(result[0][0])
                 else:
-                    if result[0][4] == 'on':
+                    if result[1] == 1:
                         for user in c.user_select(result[0][8]):
-                            content = '''Group:  %s   URL :  %s  关键字检测失败''' %(result[0][2],result[0][3])
+                            content = '''Group:  %s   URL :  %s  关键字检测失败!!!''' %(result[0][2],result[0][3])
                             m.send_mail('URL检测失败',user[0],content)
                             m.send_sms(user[1],content)
-                        m.send_sms('186xxxxxxxx',content)
-                        m.send_sms('134xxxxxxxx',content)
-                        times = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-                        with open('sms.txt','a+') as f:
-                            f.write(times + '\n')
-                            f.write(content + '\n' + '\n')
+                        m.send_sms('18610941029',content)
+                        m.send_sms('13488852824',content)
                         c.update_time(result[0][0])
                         r.redis_insert(result[0][0])
-                    else:
-                        r.redis_insert(result[0][0])
-            elif result[1] == 2:
-                if r.redis_select(result[0][0]) < 3:
-                    r.redis_insert(result[0][0])
-                else:
-                    if result[0][4] == 'on':
+                    elif result[1] == 2:
                         for user in c.user_select(result[0][8]):
-                            content = '''Group:  %s   URL :  %s  返回状态码错误''' %(result[0][2],result[0][3])
+                            content = '''Group:  %s   URL :  %s  返回状态码错误!!!''' %(result[0][2],result[0][3])
                             m.send_mail('URL检测失败',user[0],content)
                             m.send_sms(user[1],content)
-                        m.send_sms('186xxxxxxxx',content)
-                        m.send_sms('134xxxxxxxx',content)
-                        times = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-                        with open('sms.txt','a+') as f:
-                            f.write(times + '\n')
-                            f.write(content + '\n' + '\n')
+                        m.send_sms('18610941029',content)
+                        m.send_sms('13488852824',content)
                         c.update_time(result[0][0])
                         r.redis_insert(result[0][0])
-                    else:
-                        r.redis_insert(result[0][0])
-            else:
-                if r.redis_select(result[0][0]) < 3:
-                    r.redis_insert(result[0][0])
-                else:
-                    if result[0][4] == 'on':
+                    elif result[1] == 3:
                         for user in c.user_select(result[0][8]):
-                            content = '''Group:  %s   URL :  %s  返回状态码错误''' %(result[0][2],result[0][3])
+                            content = '''Group:  %s   URL :  %s  超5s 没有返回数据!!!''' %(result[0][2],result[0][3])
                             m.send_mail('URL检测失败',user[0],content)
                             m.send_sms(user[1],content)
-                        m.send_sms('186xxxxxxxx',content)
-                        m.send_sms('134xxxxxxxx',content)
-                        times = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-                        with open('sms.txt','a+') as f:
-                            f.write(times + '\n')
-                            f.write(content + '\n' + '\n')
+                        m.send_sms('18610941029',content)
+                        m.send_sms('13488852824',content)
                         c.update_time(result[0][0])
-                        r.redis_insert(result[0][0])
-                    else:
                         r.redis_insert(result[0][0])
 
 #queue.task_done()
