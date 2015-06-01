3cf8adef306e830637511b79ca9570fffb51d3f1
zhangjun <32268834@qq.com>
2015-06-01 14:25:06 +0800

    Update monitor.py
    新增未发出报警的日志记录，例如:前两次检测失败，第三次恢复正常

--- monitor.py
+++ monitor.py
@@ -2,12 +2,12 @@
 # -*- coding: utf-8 -*-
 
 import time
+from db import *
+from check_moudel import *
+from send import *
 import time,random
 from threading import Thread,Condition
 from Queue import Queue
-from db import *
-from check_moudel import *
-from wechat import *
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
@@ -26,57 +26,45 @@ class ProducerThread(Thread):
             l.append(m.check_url(x))
             queue.put(l)
 
-
 class ConsumerThread(Thread):
     def run(self):
         global queue
         for x in a:
             result = queue.get()
+            user = c.user_select(result[0][8])
             if result[0][4] != 'Yes':continue
             if result[1] == 0:
+                if r.redis_select(result[0][0]) == 0:continue
                 if r.redis_select(result[0][0]) < 3:
                     r.redis_modify(result[0][0])
+                    with open('/data/scripts/monitor/sms.txt','a+') as f:
+                        f.write('%s\t%s\t%s\n' %(result[0][1],result[0][2],result[0][3]))
+                    c.insert_log(result)
                 else:
-                    for user in c.user_select(result[0][8]):
-                        content = '''Group:  %s   URL :  %s  恢复正常''' %(result[0][2],result[0][3])
-                        m.send_mail('URL恢复正常',user[0],content)
-                        m.send_sms(user[1],content)
-                    m.send_sms('18610941029',content)
-                    m.send_sms('13488852824',content)
+		    content = '''Group:  %s   URL :  %s  恢复正常''' %(result[0][2],result[0][3])
+                    title = 'URL恢复正常'
+                    send(title,content,user)
                     r.redis_modify(result[0][0])
             elif result[1] != 0:
                 if r.redis_select(result[0][0]) < 3:
                     r.redis_insert(result[0][0])
                 else:
+                    title = 'URL检测失败'
                     if result[1] == 1:
-                        for user in c.user_select(result[0][8]):
-                            content = '''Group:  %s   URL :  %s  关键字检测失败!!!''' %(result[0][2],result[0][3])
-                            m.send_mail('URL检测失败',user[0],content)
-                            m.send_sms(user[1],content)
-                        m.send_sms('18610941029',content)
-                        m.send_sms('13488852824',content)
+                        content = '''Group:  %s   URL :  %s  关键字检测失败!!!''' %(result[0][2],result[0][3])
+                        send(title,content,user)
                         c.update_time(result[0][0])
                         r.redis_insert(result[0][0])
                     elif result[1] == 2:
-                        for user in c.user_select(result[0][8]):
-                            content = '''Group:  %s   URL :  %s  返回状态码错误!!!''' %(result[0][2],result[0][3])
-                            m.send_mail('URL检测失败',user[0],content)
-                            m.send_sms(user[1],content)
-                        m.send_sms('18610941029',content)
-                        m.send_sms('13488852824',content)
+                        content = '''Group:  %s   URL :  %s  返回状态码错误!!!''' %(result[0][2],result[0][3])
+                        send(title,content,user)
                         c.update_time(result[0][0])
                         r.redis_insert(result[0][0])
                     elif result[1] == 3:
-                        for user in c.user_select(result[0][8]):
-                            content = '''Group:  %s   URL :  %s  超5s 没有返回数据!!!''' %(result[0][2],result[0][3])
-                            m.send_mail('URL检测失败',user[0],content)
-                            m.send_sms(user[1],content)
-                        m.send_sms('18610941029',content)
-                        m.send_sms('13488852824',content)
+                        content = '''Group:  %s   URL :  %s  超5s 没有返回数据!!!''' %(result[0][2],result[0][3])
+                        send(title,content,user)
                         c.update_time(result[0][0])
                         r.redis_insert(result[0][0])
 
-#queue.task_done()
 ProducerThread().start()
-#time.sleep(1)
 ConsumerThread().start()
